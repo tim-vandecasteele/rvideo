8bbb17234a2d9103ea79b2d75cde742c281b88f4
Jonas Pfenniger <jonas@pfenniger.name>
2011-05-31 16:51:06 +0200

    Add a simple FrameCapturer spec

--- spec/integrations/frame_capturer_spec.rb
+++ spec/integrations/frame_capturer_spec.rb
@@ -2,25 +2,21 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 module RVideo
   describe FrameCapturer do
-    # TODO!
-    # These are the old specs, too tired to fix 'em up at the moment..
-    # 
-    # it "should create a screenshot with a default output path" do
-    #   output_file = "#{TEMP_PATH}/kites-10p.jpg"
-    #   FileTest.exist?(output_file).should_not be_true
-    #   file = Inspector.new(:file => spec_file("kites.mp4"))
-    #   file.capture_frame("10%").should == output_file
-    #   FileTest.exist?(output_file).should be_true
-    #   FileUtils.rm_f(output_file)
-    # end
-    # 
-    # it "should create a screenshot with a custom output path" do
-    #   output_file = spec_file("kites-10p.jpg")
-    #   FileTest.exist?(output_file).should be_false
-    #   file = Inspector.new(:file => spec_file("kites.mp4"))
-    #   file.capture_frame("10%", output_file).should == output_file
-    #   FileTest.exist?(output_file).should be_true
-    #   FileUtils.rm_f(output_file)
-    # end
+    before do
+      FileUtils.mkdir_p TEMP_PATH
+    end
+
+    after do
+      FileUtils.rm_rf TEMP_PATH
+    end
+
+    it "should create a screenshot with a custom output path" do
+      output_file = TEMP_PATH + "/kites.jpg"
+
+      file = FrameCapturer.new(:input => spec_file("kites.mp4"), :offset => "10%", :output => output_file)
+      file.capture!.should == [output_file]
+
+      FileTest.exist?(output_file).should be_true
+    end
   end
 end
