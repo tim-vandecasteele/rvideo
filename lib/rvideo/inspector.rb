5b654369847a6590c90133747e93f8653b658e0e
Tim Vandecasteele <tim.vandecasteele@gmail.com>
2012-06-04 22:30:25 +0200

    merge inspector from stable

--- lib/rvideo/inspector.rb
+++ lib/rvideo/inspector.rb
@@ -20,23 +20,23 @@ module RVideo # :nodoc:
   # check if ffmpeg is installed and exists in your operating system's path.
   class Inspector
     attr_reader :filename, :path, :full_filename, :raw_response, :raw_metadata
-    
+
     attr_accessor :ffmpeg_binary
-    
+
     def initialize(options = {})
       if not (options[:raw_response] or options[:file])
         raise ArgumentError, "Must supply either an input file or a pregenerated response"
       end
-      
+
       if options[:raw_response]
         initialize_with_raw_response(options[:raw_response])
       elsif options[:file]
         initialize_with_file(options[:file], options[:ffmpeg_binary])
       end
-      
+
       metadata = /(Input \#.*)\n(Must|At\sleast)/m.match(@raw_response)
       # metadata = /(Input \#.*)\n.+\n\Z/m.match(@raw_response)
-      
+
       if /Unknown format/i.match(@raw_response) || metadata.nil?
         @unknown_format = true
       elsif /Duration: N\/A/im.match(@raw_response)
@@ -47,11 +47,11 @@ module RVideo # :nodoc:
         @raw_metadata = metadata[1]
       end
     end
-    
+
     def initialize_with_raw_response(raw_response)
       @raw_response = raw_response
     end
-    
+
     def initialize_with_file(file, ffmpeg_binary = nil)
       if ffmpeg_binary
         @ffmpeg_binary = ffmpeg_binary
@@ -65,48 +65,48 @@ module RVideo # :nodoc:
         end
         @ffmpeg_binary = "ffmpeg"
       end
-      
+
       if not FileTest.exist?(file.gsub('"',''))
         raise TranscoderError::InputFileNotFound, "File not found (#{file})"
       end
-      
+
       @full_filename = file
       @filename      = File.basename(@full_filename)
       @path          = File.dirname(@full_filename)
-      
+
       @raw_response = `#{@ffmpeg_binary} -i #{@full_filename.shell_quoted} 2>&1`
     end
-    
+
     # Returns true if the file can be read successfully. Returns false otherwise.
     def valid?
       not (@unknown_format or @unreadable_file)
     end
-    
+
     # Returns false if the file can be read successfully. Returns false otherwise.
     def invalid?
       not valid?
     end
-    
+
     # True if the format is not understood ("Unknown Format")
     def unknown_format?
       @unknown_format ? true : false
     end
-    
+
     # True if the file is not readable ("Duration: N/A, bitrate: N/A")
     def unreadable_file?
       @unreadable_file ? true : false
     end
-    
+
     # Does the file have an audio stream?
     def audio?
       not audio_match.nil?
     end
-    
+
     # Does the file have a video stream?
     def video?
       not video_match.nil?
     end
-    
+
     # Returns the version of ffmpeg used, In practice, this may or may not be 
     # useful.
     #
@@ -118,7 +118,7 @@ module RVideo # :nodoc:
     def ffmpeg_version
       @ffmpeg_version = @raw_response.split("\n").first.split("version").last.split(",").first.strip
     end
-    
+
     # Returns the configuration options used to build ffmpeg.
     #
     # Example:
@@ -129,7 +129,7 @@ module RVideo # :nodoc:
     def ffmpeg_configuration 
       /(\s*configuration:)(.*)\n/.match(@raw_response)[2].strip
     end
-    
+
     # Returns the versions of libavutil, libavcodec, and libavformat used by
     # ffmpeg.
     #
@@ -142,7 +142,7 @@ module RVideo # :nodoc:
     def ffmpeg_libav
       /^(\s*lib.*\n)+/.match(@raw_response)[0].split("\n").each {|l| l.strip! }
     end
-    
+
     # Returns the build description for ffmpeg.
     #
     # Example:
@@ -153,7 +153,7 @@ module RVideo # :nodoc:
     def ffmpeg_build
       /(\n\s*)(built on.*)(\n)/.match(@raw_response)[2]
     end
-    
+
     # Returns the container format for the file. Instead of returning a single
     # format, this may return a string of related formats.
     #
@@ -167,7 +167,7 @@ module RVideo # :nodoc:
       return nil if @unknown_format
       /Input \#\d+\,\s*(\S+),\s*from/.match(@raw_metadata)[1]
     end
-    
+
     # The duration of the movie, as a string.
     #
     # Example:
@@ -178,7 +178,7 @@ module RVideo # :nodoc:
       return nil unless valid?
       /Duration:\s*([0-9\:\.]+),/.match(@raw_metadata)[1]
     end
-    
+
     # The duration of the movie in milliseconds, as an integer.
     #
     # Example: 
@@ -191,16 +191,16 @@ module RVideo # :nodoc:
     #
     def duration
       return nil unless valid?
-      
+
       units = raw_duration.split(":")
       (units[0].to_i * 60 * 60 * 1000) + (units[1].to_i * 60 * 1000) + (units[2].to_f * 1000).to_i
     end
-    
+
     def ratio
       return nil unless valid?
       width.to_f / height.to_f
     end
-    
+
     # The bitrate of the movie.
     #
     # Example:
@@ -211,7 +211,7 @@ module RVideo # :nodoc:
       return nil unless valid?
       bitrate_match[1].to_i
     end
-    
+
     # The bitrate units used. In practice, this may always be kb/s.
     #
     # Example:
@@ -222,32 +222,32 @@ module RVideo # :nodoc:
       return nil unless valid?
       bitrate_match[2]
     end
-    
+
     def bitrate_with_units
       "#{bitrate} #{bitrate_units}"
     end
-    
+
     def audio_bit_rate
       return nil unless audio?
       audio_match[7].to_i
     end
-    
+
     def audio_bit_rate_units
       return nil unless audio?
       audio_match[8]
     end
-    
+
     def audio_bit_rate_with_units
       "#{audio_bit_rate} #{audio_bit_rate_units}"
     end
-    
+
     def audio_stream
       return nil unless valid?
-      
+
       match = /\n\s*Stream.*Audio:.*\n/.match(@raw_response)
       match[0].strip if match
     end
-    
+
     # The audio codec used.
     #
     # Example:
@@ -258,7 +258,7 @@ module RVideo # :nodoc:
       return nil unless audio?
       audio_match[2]
     end
-    
+
     # The sampling rate of the audio stream.
     #
     # Example:
@@ -269,7 +269,7 @@ module RVideo # :nodoc:
       return nil unless audio?
       audio_match[3].to_i
     end
-    
+
     # The units used for the sampling rate. May always be Hz.
     #
     # Example:
@@ -281,11 +281,11 @@ module RVideo # :nodoc:
       audio_match[4]
     end
     alias_method :audio_sample_units, :audio_sample_rate_units
-    
+
     def audio_sample_rate_with_units
       "#{audio_sample_rate} #{audio_sample_rate_units}"
     end
-    
+
     # The channels used in the audio stream.
     #
     # Examples:
@@ -297,10 +297,10 @@ module RVideo # :nodoc:
       return nil unless audio?
       audio_match[5]
     end
-    
+
     def audio_channels
       return nil unless audio?
-      
+
       case audio_match[5]
       when "mono"   then 1
       when "stereo" then 2
@@ -310,14 +310,14 @@ module RVideo # :nodoc:
         raise RuntimeError, "Unknown number of channels"
       end
     end
-    
+
     # This should almost always return 16, 
     # as the vast majority of audio is 16 bit.
     def audio_sample_bit_depth
       return nil unless audio?
       audio_match[6].to_i
     end
-    
+
     # The ID of the audio stream (useful for troubleshooting).
     #
     # Example:
@@ -327,14 +327,15 @@ module RVideo # :nodoc:
       return nil unless audio?
       audio_match[1]
     end
-    
+
     def video_stream
       return nil unless valid?
-      
+
       match = /\n\s*Stream.*Video:.*\n/.match(@raw_response)
       match[0].strip unless match.nil?
     end
-    
+
+#rspeced
     # The ID of the video stream (useful for troubleshooting).
     #
     # Example:
@@ -344,7 +345,8 @@ module RVideo # :nodoc:
       return nil unless video?
       video_match[1]
     end
-    
+
+#rspeced
     # The video codec used.
     #
     # Example:
@@ -353,9 +355,10 @@ module RVideo # :nodoc:
     #
     def video_codec
       return nil unless video?
-      video_match[3]
+      video_match[2]
     end
-    
+
+#rspeced
     # The colorspace of the video stream.
     #
     # Example:
@@ -364,29 +367,32 @@ module RVideo # :nodoc:
     #
     def video_colorspace
       return nil unless video?
-      video_match[4]
+      video_match[3]
     end
-    
+
+#rspeced
     # The width of the video in pixels.
     def width
       return nil unless video?
-      if transposed?
-        video_match[6].to_i
-      else
+      if aspect_rotated?
         video_match[5].to_i
+      else
+        video_match[4].to_i
       end
     end
-    
+
+#rspeced
     # The height of the video in pixels.
     def height
       return nil unless video?
-      if transposed?
-        video_match[5].to_i
+      if aspect_rotated?
+        video_match[4].to_i
       else
-        video_match[6].to_i
+        video_match[5].to_i
       end
     end
-    
+
+#rspeced
     # width x height, as a string.
     #
     # Examples:
@@ -397,45 +403,50 @@ module RVideo # :nodoc:
       return nil unless video?
       "#{width}x#{height}"
     end
-    
+
     def video_orientation
-      return @orientation if @orientation
-      child = POSIX::Spawn::Child.new("qtrotate #{full_filename}", :timeout => 10)
-      @orientation = child.out.chomp.to_i
-    rescue => ex
-      RVideo.logger.error(ex)
+      stdout=''
+      stderr=''
+      open4.spawn "qtrotate #{full_filename}", :stdout=> stdout, :timeout => 10, :stderr => stderr
+      @orientation ||= stdout.chomp.to_i
+    rescue Timeout::Error
+      0
+    rescue
       0
     end
-    
-    def transposed?
-      video_orientation % 180 == 90
-    end
-    
+
     def rotated?
       video_orientation != 0
     end
-    
+
+    def aspect_rotated?
+      video_orientation % 180 == 90
+    end
+#rspeced
     def pixel_aspect_ratio
+      return nil unless video?
+      video_match[6]
+    end
+
+#rspeced
+    def display_aspect_ratio
       return nil unless video?
       video_match[7]
     end
-    
-    def display_aspect_ratio
-      return nil unless video?
-      video_match[8]
-    end
-    
+#rspeced
     # The portion of the overall bitrate the video is responsible for.
     def video_bit_rate
       return nil unless video?
-      video_match[9]
+      video_match[8]
     end
-    
+
+#rspeced
     def video_bit_rate_units
       return nil unless video?
-      video_match[10]
+      video_match[9]
     end
-    
+
+#rspeced
     # The frame rate of the video in frames per second
     #
     # Example:
@@ -444,74 +455,72 @@ module RVideo # :nodoc:
     #
     def fps
       return nil unless video?
-      fps = video_match[2] || video_match[13] || video_match[14]
-      fps = fps.to_f * 1000 if fps =~ /\d+k/i
-      fps = fps.to_f if fps
-      fps
+      video_match[12] || video_match[13]
     end
     alias_method :framerate, :fps
     
+#rspeced
     def time_base
       return nil unless video?
       video_match[14]
     end
-    
+
+#rspeced
     def codec_time_base
       return nil unless video?
       video_match[15]
     end
-    
+
     private
 
     def bitrate_match
       /bitrate: ([0-9\.]+)\s*(.*)\s+/.match(@raw_metadata)
     end
-    
+
     ###
     # I am wondering how reliable it would be to simplify a lot 
     # of this regexp parsery by using split(/\s*,\s*/) - Seth
-    
+
     SEP = '(?:,\s*)'
     VAL = '([^,]+)'
-    
+
     RATE = '([\d.]+k?)'
-    PAR_DAR = '(?:\s*\[?(?:PAR\s*(\d+:\d+))?\s*(?:DAR\s*(\d+:\d+))?\]?)?'
-    
+    PAR_DAR = '(?:\s*\[?(?:SAR\s*(\d+:\d+))?\s*(?:DAR\s*(\d+:\d+))?\]?)?'
+
     AUDIO_MATCH_PATTERN = /
-      Stream\s+(.*?)[,:\(\[].*?\s*
-      Audio:\s+
-      #{VAL}#{SEP}           # codec
-      #{RATE}\s+(\w*)#{SEP}? # sample rate
-      #{VAL}#{SEP}?          # channels
-      (?:s(\d+)#{SEP}?)?     # audio sample bit depth
-      (?:(\d+)\s+(\S+))?     # audio bit rate
-    /x
-    
-    def audio_match
-      return nil unless valid?
-      AUDIO_MATCH_PATTERN.match(audio_stream)
+    Stream\s+(.*?)[,:\(\[].*?\s*
+                       Audio:\s+
+                       #{VAL}#{SEP}           # codec
+                       #{RATE}\s+(\w*)#{SEP}? # sample rate
+                       #{VAL}#{SEP}?          # channels
+                       (?:s(\d+)#{SEP}?)?     # audio sample bit depth
+                       (?:(\d+)\s+(\S+))?     # audio bit rate
+                       /x
+
+                       def audio_match
+                         return nil unless valid?
+                         AUDIO_MATCH_PATTERN.match(audio_stream)
     end
-    
+
     FPS = 'fps(?:\(r\))?'
-    
+
     VIDEO_MATCH_PATTERN = /
-      Stream\s*(\#[\d.]+)(?:[\(\[].+?[\)\]])?\s*                  # stream id
-      [,:]\s*                                                     
-      (?:#{RATE}\s*#{FPS}[,:]\s*)?                                # frame rate, older builds
-      Video:\s*                                                   
-      #{VAL}#{SEP}                                                # codec
-      (?:#{VAL}#{SEP})?                                           # color space
-      (\d+)x(\d+)                                                 # resolution
-      #{PAR_DAR}                                                  # pixel and display aspect ratios
-      #{SEP}?
-      (?:#{RATE}\s*(kb\/s)#{SEP}?)?                               # video bit rate
-      #{PAR_DAR}#{SEP}?                                           # pixel and display aspect ratios
-      (?:#{RATE}\s*#{FPS}#{SEP}?)?                                # frame rate
-      (?:#{RATE}\s*tbr#{SEP}?)?                                   # time base
-      (?:#{RATE}\s*tbn#{SEP}?)?                                   # time base
-      (?:#{RATE}\s*tbc#{SEP}?)?                                   # codec time base
+    Stream\s*(\#[\d.]+:[\d.]+)(?:[\(\[].+?[\)\]])?\s*                  # stream id
+    [:]\s*                                                     
+    Video:\s*                                                   
+    #{VAL}#{SEP}                                                # codec
+    (?:#{VAL}#{SEP})?                                           # color space
+    (\d+)x(\d+)                                                 # resolution
+    #{PAR_DAR}                                                  # pixel and display aspect ratios
+    #{SEP}?
+    (?:#{RATE}\s*(kb\/s)#{SEP}?)?                               # video bit rate
+    #{PAR_DAR}#{SEP}?                                           # pixel and display aspect ratios
+    (?:#{RATE}\s*#{FPS}#{SEP}?)?                                # frame rate
+    (?:#{RATE}\s*tbr#{SEP}?)?                                   # time base
+    (?:#{RATE}\s*tbn#{SEP}?)?                                   # time base
+    (?:#{RATE}\s*tbc#{SEP}?)?                                   # codec time base
     /x
-    
+
     def video_match
       return nil unless valid?
       VIDEO_MATCH_PATTERN.match(video_stream)
